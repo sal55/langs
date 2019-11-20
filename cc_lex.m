caf6c09690f6b64cf319682c02f11e66fba02253
sal55 <47458439+sal55@users.noreply.github.com>
2019-11-20 22:29:45 +0000

    Update cc_lex.m

--- cc_lex.m
+++ cc_lex.m
@@ -11,39 +11,7 @@ import cc_tables
 import cc_headers
 import cc_lib
 
-!(this struct def exists in cc_decls but is duplicated here for this demo file)
-global record tokenrec =        !should be 32-byte record
-    union
-        var int64 value             !64-bit int
-        var real xvalue             !64-bit float
-        var word64 uvalue           !64-bit word
-        var ref char svalue         !pointer to string or charconst (not terminated)
-        var ref strec symptr        !pointer to symbol table entry for name
-    end
-    var ref tokenrec nexttoken
-
-    union
-        struct
-            var byte subcode
-            var byte flags
-        end
-        var word16 subcodex
-    end
-    var byte symbol
-    var byte fileno
-
-    var word32 lineno
-
-    var int32 length                    !length of name/string/char
-    union
-        var int32 numberoffset      !offset of numeric token within file[fileno]
-        var int16 paramno               !for macro params
-        var int16 pasteno
-    end
-end
-
-
-var ref tokenrec tkptr=nil
+var ref tokenrec tkptr=nil    !! for tokenrec details, see https://github.com/sal55/langs/blob/master/tokenrec.m
 
 var int dowhitespace=0
 
