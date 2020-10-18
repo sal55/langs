7eb6b8c6e10ca84080737ff1d0b8b1c32e6c729a
sal55 <47458439+sal55@users.noreply.github.com>
2020-10-18 22:59:14 +0100

    Update evalexpr.c

--- evalexpr.c
+++ evalexpr.c
@@ -114,13 +114,7 @@ static void nexttk(void) {
     case '/': token=div_tk; break;
     case '(': token=lbrack_tk; break;
     case ')': token=rbrack_tk; break;
-    case 'A':case 'B':case 'C':case 'D':case 'E':case 'F':case 'G':
-    case 'H':case 'I':case 'J':case 'K':case 'L':case 'M':case 'N':
-    case 'O':case 'P':case 'Q':case 'R':case 'S':case 'T':case 'U':
-    case 'V':case 'W':case 'X':case 'Y':case 'Z':
-        tkvalue=variables[(c-'A')];
-        token=rbrack_tk; break;
-
+                
     case 'a':case 'b':case 'c':case 'd':case 'e':case 'f':case 'g':
     case 'h':case 'i':case 'j':case 'k':case 'l':case 'm':case 'n':
     case 'o':case 'p':case 'q':case 'r':case 's':case 't':case 'u':
