0cdbf20e4734ec213b557ef71685247cd7d11e61
sal55 <47458439+sal55@users.noreply.github.com>
2021-07-12 13:50:11 +0100

    Create binary.c

---
+++ binary.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+long long int maxalloc, curralloc;
+
+typedef struct _node {
+    struct _node *left;
+    struct _node *right;
+    int item;
+} treenode;
+
+
+treenode* newtreenode(treenode* left, treenode* right, int item) {
+    treenode* t;
+
+    t=malloc(sizeof(treenode));
+    ++curralloc;
+    if (curralloc>maxalloc) maxalloc=curralloc;
+
+    t->left=left;
+    t->right=right;
+    t->item=item;
+    return t;
+}
+
+int itemcheck(treenode* tree) {
+    if (tree->left==NULL)
+        return tree->item;
+    else
+        return tree->item+itemcheck(tree->left)-itemcheck(tree->right);
+}
+
+treenode* bottomuptree(int item, int depth) {
+    if (depth>0)
+        return newtreenode(bottomuptree(2*item-1,depth-1),
+                bottomuptree(2*item,depth-1),item);
+    else
+        return newtreenode(NULL,NULL,item);
+}
+
+void deletetree(treenode* tree) {
+    if (tree->left) {
+        deletetree(tree->left);
+        deletetree(tree->right);
+    }
+    free(tree); --curralloc;
+}
+
+int main(void) {
+    treenode *stretchtree, *longlivedtree, *temptree;
+    int n,depth,mindepth,maxdepth,stretchdepth,check,iterations,i;
+
+    n=18;
+    mindepth=4;
+    if ((mindepth+2)>n)
+        maxdepth=mindepth+1;
+    else
+        maxdepth=n;
+
+    stretchdepth=maxdepth+1;
+
+    stretchtree=bottomuptree(0,stretchdepth);
+    printf("Stretch tree of depth %d    check: %d\n",stretchdepth,itemcheck(stretchtree));
+
+    deletetree(stretchtree);
+    longlivedtree=bottomuptree(0,maxdepth);
+
+    depth=mindepth;
+    while (depth<=maxdepth) {
+        iterations=1<<(maxdepth-depth+mindepth);
+        check=0;
+        for (i=1; i<=iterations; ++i) {
+            temptree=bottomuptree(i,depth);
+            check=check+itemcheck(temptree);
+            deletetree(temptree);
+
+            temptree=bottomuptree(-i,depth);
+            check=check+itemcheck(temptree);
+            deletetree(temptree);
+        }
+        printf("%d  Trees of depth %d check: %d\n",iterations*2,depth,check);
+        depth=depth+2;
+    }
+    printf("%s %d %s %d\n","long lived tree of depth",maxdepth,"\tcheck:",itemcheck(longlivedtree));
+    printf("maxalloc = %lld\n",maxalloc*sizeof(treenode));
+}
