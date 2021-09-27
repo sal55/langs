4b44307dbbdf5909b8b92c1da506fee2e897e7b2
sal55 <47458439+sal55@users.noreply.github.com>
2021-09-27 11:44:34 +0100

    Create test.c

---
+++ pcl/test.c
@@ -0,0 +1,15 @@
+/* Generated C */
+#include "pclhdr.h"
+
+extern i32 printfx(u64,...);
+void test_start(void);
+
+void test_start(void) {
+    u64 S1, S2, S3, S4, S5, S6;
+    S1 = (u64)"Hello, World! [pcl2]\n";
+    printf(S1);
+    S1 = 0;
+    exit(S1);
+}
+
+void start(void) {test_start();}
