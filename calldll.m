7db7b416b6d97357ad5448c6b8b1efa91075dfd8
sal55 <47458439+sal55@users.noreply.github.com>
2022-08-08 11:05:54 +0100

    Update calldll.m

--- calldll.m
+++ calldll.m
@@ -1,30 +1,15 @@
-export function os_calldllfunction(ref proc fnaddr, int retcode, nargs, ref[]i64 args, ref[]byte argcodes)word64 =
+export function os_calldllfunction(ref proc fnaddr,
+        int retcode, nargs, ref[]i64 args, ref[]byte argcodes)word64 =
     word64 a
     real64 x
-    int oddstack, nextra, pushedbytes
+    int nextra, pushedbytes
 
-    oddstack:=nextra:=0
+    nextra:=0
 
-    assem
-        test astack,8
-        jz L100
-        mov byte [oddstack],1
-L100:
-    end
-
-    if oddstack then
-        if nargs<5 then
-            nextra:=5-nargs
-        elsif nargs.even then
-            nextra:=1
-        fi
-
-    else
-        if nargs<4 then
-            nextra:=4-nargs
-        elsif nargs.odd then
-            nextra:=1
-        fi
+    if nargs<4 then
+        nextra:=4-nargs         !need at least 4 slots for shadow space
+    elsif nargs.odd then        !need one more for a 16-byte-aligned stack
+        nextra:=1
     fi
 
     pushedbytes:=(nextra+nargs)*8
@@ -38,8 +23,11 @@ L100:
         asm push word64 [a]
     od
 
-! Load first 4 args to registers; this version will blindly load 4 args
-! even if there are fewer) to both integer and xmm registers.
+!load first 4 args to registers; this first version will blindly load 4 args
+!(even if there are less) to both integer and xmm registers. Should be int/pointer
+!types to integer regs; float types to xmm; and variadic to both
+!This requires the flags in argcodes[], currently not used
+
     assem
         mov D10,[Dstack]
         movq XMM0,[Dstack]
