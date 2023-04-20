270b59dc4c72981c1fed44a7b77dddfd7114a136
sal55 <47458439+sal55@users.noreply.github.com>
2023-04-20 20:16:18 +0100

    Create sw.m

---
+++ temp/sw.m
@@ -0,0 +1,94 @@
+!Switch with compiler-generated jumptable
+
+[100]i64 stack
+ref i64 sptr
+int pcindex
+
+i64 x=0
+i64 y=1
+
+enumdata =
+    kloadimm,
+    kload,
+    kstore,
+    kadd,
+    kjumpne,
+    kunload,
+    kstop,
+end
+
+tabledata []byte code, []int data =
+    (kloadimm,      42),
+    (kloadimm,      56),
+    (kadd,          0),
+    (kunload,       0),
+
+    (kload,         int(&x)),
+    (kload,         int(&y)),
+    (kadd,          0),
+    (kstore,        int(&x)),
+
+    (kload,         int(&x)),
+    (kloadimm,      100'000'000),
+    (kjumpne,       1),
+    (kstop,         0),
+end
+
+proc run=
+    ref i64 a
+    ref i64 sp
+    int pc
+
+
+    pc:=1
+    sp:=cast(&stack)
+
+    doswitchu code[pc]
+    when kloadimm then
+        ++sp
+        sp^:=data[pc]
+        ++pc
+
+    when kload then
+        a:=cast(data[pc])
+        ++sp
+        sp^:=a^
+        ++pc
+
+    when kstore then
+        a:=cast(data[pc])
+        a^:=sp^
+        --sp
+        ++pc
+
+    when kadd then
+        --sp
+        sp^ +:= (sp+1)^
+        ++pc
+
+    when kjumpne then
+        sp:=sp-2
+        if (sp+1)^ <> (sp+2)^ then
+            pc := data[pc]
+        else
+            ++pc
+        fi
+
+    when kunload then
+        --sp
+        ++pc
+    when kstop then
+        exit
+    else
+        println "Umimpl op", CODE[PCINDEX]
+        stop
+    end
+
+    println "Stop",=x,=y
+    stop
+
+end
+
+proc main=
+    run()
+end
