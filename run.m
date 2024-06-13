7d54cd3d3487acf118c093a8e51c69d083bfb695
sal55 <47458439+sal55@users.noreply.github.com>
2024-06-13 19:23:53 +0100

    Update run.m

--- run.m
+++ run.m
@@ -1,236 +1,251 @@
-enumdata []ichar opcodenames =
-    (kadd,      $),
-    (kneg,      $),
-    (kmul,      $),
-    (kdivd,     $),
-    (kremd,     $),
-    (kdiv2,     $),
-    (krem2,     $),
-    (keqli,     $),
-    (kneqi,     $),
-    (klssi,     $),
-    (kleqi,     $),
-    (kgtri,     $),
-    (kgeqi,     $),
-    (kdupl,     $),
-    (kswap,     $),
-    (kandb,     $),
-    (korb,      $),
-    (kload,     $),
-    (kstor,     $),
-    (khalt,     $),
-    (kwri,      $),
-    (kwrc,      $),
-    (kwrl,      $),
-    (krdi,      $),
-    (krdc,      $),
-    (krdl,      $),
-    (keol,      $),
+function run()int64 =
+    ref CallFrame frame
+    byte instruction
+    word64 constant
+    byte slot
+    ref ObjString name
+    word64 value
+    ref ObjInstance instance
+    ref ObjClass superclass
+    word64 b
+    word64 a
+    word16 offset
+    int32 argCount
+    ref ObjString method
+    ref ObjFunction function
+    ref ObjClosure closure
+    int32 i
+    byte isLocal
+    byte index
+    word64 result
+    ref ObjClass subclass
 
-    (kldc,      $),     !instrs from here on have an inline operand
-    (kldla,     $),
-    (kldl,      $),
-    (kldg,      $),
-    (kstl,      $),
-    (kstg,      $),
-    (kmove,     $),
-    (kcopy,     $),
-    (kaddc,     $),
-    (kmulc,     $),
-    (kjump,     $),
-    (kjumpz,    $),
-    (kcall,     $),
-    (kadjs,     $),
-    (ksets,     $),
-    (kexit,     $),
-end
-
-[0..1000]int m
-
-[0:]int code = (
-    include "fib.bc"
-)
-
-proc main =
-    interpret()
-    println
-    println
-end
- 
-proc error(ichar mess)=
-    println "Runime error",mess
-    stop 1
-end
-
-proc interpret =
-    int pc, sp, ia
-    int c, k, j, n
-
-    pc:=0
-
-    doswitchu ia:=code[pc+1]; pc+:=2; code[pc-2]
-    when kadd  then
-        m[sp+1]:=m[sp+1]+m[sp]
-        ++sp
-
-    when kneg  then
-        m[sp]:=-m[sp]
-
-    when kmul  then
-        m[sp+1]:=m[sp+1]*m[sp]
-        ++sp
-
-    when kdivd then
-        m[sp+1]:=m[sp+1] / m[sp]
-        ++sp
-
-    when kremd then
-        m[sp+1]:=m[sp+1] rem m[sp]
-        ++sp
-
-    when kdiv2 then
-        m[sp]:=m[sp] % 2
-
-    when krem2 then
-        m[sp]:=m[sp] rem 2
-
-    when keqli then
-        m[sp+1]:=m[sp+1]=m[sp]
-        ++sp
-
-    when kneqi then
-        m[sp+1]:=m[sp+1]<>m[sp]
-        ++sp
-
-    when klssi then
-        m[sp+1]:=m[sp+1]<m[sp]
-        ++sp
-
-    when kleqi then
-        m[sp+1]:=m[sp+1]<=m[sp]
-        ++sp
-
-    when kgtri then
-        m[sp+1]:=m[sp+1]>m[sp]
-        ++sp
-
-    when kgeqi then
-        m[sp+1]:=m[sp+1]>=m[sp]
-        ++sp
-
-    when kdupl then
-        --sp
-        m[sp]:=m[sp+1]
-
-    when kswap then
-        swap(m[sp], m[sp+1])
-
-    when kandb then
-        if m[sp]=0 then m[sp+1]:=0 fi
-        ++sp
-
-    when korb  then
-        if m[sp]=1 then m[sp+1]:=1 fi
-        ++sp
-
-    when kload then
-        m[sp]:=m[m[sp]]
-
-    when kstor then
-        m[m[sp]]:=m[sp+1]
-        sp+:=2
-
-    when khalt then
-        exit
-
-    when kwri  then
-        print m[sp+1]:"8 jr"
-        sp+:=2
-
-    when kwrc  then
-        print char(m[sp])
-        ++sp
-
-    when kwrl  then
-        println
-
-    when krdi  then
-        read m[m[sp]]
-        ++sp
-
-    when krdc  then
-        m[m[sp]]:=c
-        ++sp
-
-    when krdl  then
-        error("RDL")
-
-    when keol  then
-        --sp
-        error("eol")
-
-    when kldc  then
-        --sp
-        m[sp]:=ia
-    when kldla then
-        --sp
-        m[sp]:=sp+1+ia
-
-    when kldl  then
-        --sp
-        m[sp]:=m[sp+1+ia]
-
-    when kldg  then
-        --sp
-        m[sp]:=m[ia]
-
-    when kstl  then
-        m[sp+ia]:=m[sp]
-        ++sp
-
-    when kstg  then
-        m[ia]:=m[sp]
-        ++sp
-
-    when kmove then
-        k:=m[sp]
-        j:=m[sp+1]
-        sp+:=2
-        n:=ia
-        repeat --n; m[k+n]:=m[j+n] until n=0
-
-    when kcopy then
-        j:=m[sp]
-        n:=ia
-        sp:=sp-n+1
-        repeat --n; m[sp+n]:=m[j+n] until n=0
-
-    when kaddc then
-        m[sp]:=m[sp]+ia
-
-    when kmulc then
-        m[sp]:=m[sp]*ia
-
-    when kjump then
-        pc:=ia
-
-    when kjumpz then
-        if m[sp]=0 then pc:=ia fi
-        ++sp
-
-    when kcall then
-        --sp
-        m[sp]:=pc
-        pc:=ia
-
-    when kadjs then
-        sp:=sp+ia
-
-    when ksets then
-        sp:=ia
-
-    when kexit then
-        pc:=m[sp]
-        sp+:=ia
-    end doswitchu
+    frame := &vm.frames[vm.frameCount-1]
+    do
+        switch instruction := (frame^.ip++)^
+        when OP_CONSTANT then
+            constant := ((frame^.closure)^.function)^.chunk.constants.values[(frame^.ip++)^]
+            pushx(constant)
+        when OP_NIL then
+            pushx(word64(word64(9222246136947933184) ior 1))
+        when OP_TRUE then
+            pushx((1|word64(word64(9222246136947933184) ior 3)|word64(word64(9222246136947933184) ior 2)))
+        when OP_FALSE then
+            pushx((0|word64(word64(9222246136947933184) ior 3)|word64(word64(9222246136947933184) ior 2)))
+        when OP_POP then
+            popx()
+        when OP_GET_LOCAL then
+            slot := (frame^.ip++)^
+            pushx(frame^.slots[slot])
+        when OP_SET_LOCAL then
+            slot := (frame^.ip++)^
+            frame^.slots[slot] := peek(0)
+        when OP_GET_GLOBAL then
+            name := ref ObjString(ref Obj(word64(((frame^.closure)^.function)^.chunk.constants.values[(frame^.ip++)^] iand inot word64(-9223372036854775808) ior word64(9222246136947933184))))
+            if not tableGet(&vm.globals,name,&value) then
+                runtimeError("Undefined variable '%s'.",name^.chars)
+                return INTERPRET_RUNTIME_ERROR
+            fi
+            pushx(value)
+        when OP_DEFINE_GLOBAL then
+            name := ref ObjString(ref Obj(word64(((frame^.closure)^.function)^.chunk.constants.values[(frame^.ip++)^] iand inot word64(-9223372036854775808) ior word64(9222246136947933184))))
+            tableSet(&vm.globals,name,peek(0))
+            popx()
+        when OP_SET_GLOBAL then
+            name := ref ObjString(ref Obj(word64(((frame^.closure)^.function)^.chunk.constants.values[(frame^.ip++)^] iand inot word64(-9223372036854775808) ior word64(9222246136947933184))))
+            if tableSet(&vm.globals,name,peek(0)) then
+                tableDelete(&vm.globals,name)
+                runtimeError("Undefined variable '%s'.",name^.chars)
+                return INTERPRET_RUNTIME_ERROR
+            fi
+        when OP_GET_UPVALUE then
+            slot := (frame^.ip++)^
+            pushx((((frame^.closure)^.upvalues[slot])^.location)^)
+        when OP_SET_UPVALUE then
+            slot := (frame^.ip++)^
+            (((frame^.closure)^.upvalues[slot])^.location)^ := peek(0)
+        when OP_GET_PROPERTY then
+            if not isObjType(peek(0),OBJ_INSTANCE) then
+                runtimeError("Only instances have properties.")
+                return INTERPRET_RUNTIME_ERROR
+            fi
+            instance := ref ObjInstance(ref Obj(word64(peek(0) iand inot word64(-9223372036854775808) ior word64(9222246136947933184))))
+            name := ref ObjString(ref Obj(word64(((frame^.closure)^.function)^.chunk.constants.values[(frame^.ip++)^] iand inot word64(-9223372036854775808) ior word64(9222246136947933184))))
+            if tableGet(&instance^.fields,name,&value) then
+                popx()
+                pushx(value)
+            fi
+            if not bindMethod(instance^.klass,name) then
+                return INTERPRET_RUNTIME_ERROR
+            fi
+        when OP_SET_PROPERTY then
+            if not isObjType(peek(1),OBJ_INSTANCE) then
+                runtimeError("Only instances have fields.")
+                return INTERPRET_RUNTIME_ERROR
+            fi
+            instance := ref ObjInstance(ref Obj(word64(peek(1) iand inot word64(-9223372036854775808) ior word64(9222246136947933184))))
+            tableSet(&instance^.fields,ref ObjString(ref Obj(word64(((frame^.closure)^.function)^.chunk.constants.values[(frame^.ip++)^] iand inot word64(-9223372036854775808) ior word64(9222246136947933184)))),peek(0))
+            value := popx()
+            popx()
+            pushx(value)
+        when OP_GET_SUPER then
+            name := ref ObjString(ref Obj(word64(((frame^.closure)^.function)^.chunk.constants.values[(frame^.ip++)^] iand inot word64(-9223372036854775808) ior word64(9222246136947933184))))
+            superclass := ref ObjClass(ref Obj(word64(popx() iand inot word64(-9223372036854775808) ior word64(9222246136947933184))))
+            if not bindMethod(superclass,name) then
+                return INTERPRET_RUNTIME_ERROR
+            fi
+        when OP_EQUAL then
+            b := popx()
+            a := popx()
+            pushx((valuesEqual(a,b)|word64(word64(9222246136947933184) ior 3)|word64(word64(9222246136947933184) ior 2)))
+        when OP_GREATER then
+            repeat 
+                if not peek(0) iand word64(9222246136947933184)<>word64(9222246136947933184) or not peek(1) iand word64(9222246136947933184)<>word64(9222246136947933184) then
+                    runtimeError("Operands must be numbers.")
+                    return INTERPRET_RUNTIME_ERROR
+                fi
+                b := valueToNum(popx())
+                a := valueToNum(popx())
+                pushx((a>b|word64(word64(9222246136947933184) ior 3)|word64(word64(9222246136947933184) ior 2)))
+            until not(0)
+        when OP_LESS then
+            repeat 
+                if not peek(0) iand word64(9222246136947933184)<>word64(9222246136947933184) or not peek(1) iand word64(9222246136947933184)<>word64(9222246136947933184) then
+                    runtimeError("Operands must be numbers.")
+                    return INTERPRET_RUNTIME_ERROR
+                fi
+                b := valueToNum(popx())
+                a := valueToNum(popx())
+                pushx((a<b|word64(word64(9222246136947933184) ior 3)|word64(word64(9222246136947933184) ior 2)))
+            until not(0)
+        when OP_ADD then
+            if isObjType(peek(0),OBJ_STRING) and isObjType(peek(1),OBJ_STRING) then
+                concatenate()
+            else
+                if peek(0) iand word64(9222246136947933184)<>word64(9222246136947933184) and peek(1) iand word64(9222246136947933184)<>word64(9222246136947933184) then
+                    b := valueToNum(popx())
+                    a := valueToNum(popx())
+                    pushx(numToValue(a+b))
+                else
+                    runtimeError("Operands must be two numbers or two strings.")
+                    return INTERPRET_RUNTIME_ERROR
+                fi
+            fi
+        when OP_SUBTRACT then
+            repeat 
+                if not peek(0) iand word64(9222246136947933184)<>word64(9222246136947933184) or not peek(1) iand word64(9222246136947933184)<>word64(9222246136947933184) then
+                    runtimeError("Operands must be numbers.")
+                    return INTERPRET_RUNTIME_ERROR
+                fi
+                b := valueToNum(popx())
+                a := valueToNum(popx())
+                pushx(numToValue(a-b))
+            until not(0)
+        when OP_MULTIPLY then
+            repeat 
+                if not peek(0) iand word64(9222246136947933184)<>word64(9222246136947933184) or not peek(1) iand word64(9222246136947933184)<>word64(9222246136947933184) then
+                    runtimeError("Operands must be numbers.")
+                    return INTERPRET_RUNTIME_ERROR
+                fi
+                b := valueToNum(popx())
+                a := valueToNum(popx())
+                pushx(numToValue(a*b))
+            until not(0)
+        when OP_DIVIDE then
+            repeat 
+                if not peek(0) iand word64(9222246136947933184)<>word64(9222246136947933184) or not peek(1) iand word64(9222246136947933184)<>word64(9222246136947933184) then
+                    runtimeError("Operands must be numbers.")
+                    return INTERPRET_RUNTIME_ERROR
+                fi
+                b := valueToNum(popx())
+                a := valueToNum(popx())
+                pushx(numToValue(a/b))
+            until not(0)
+        when OP_NOT then
+            pushx((isFalsey(popx())|word64(word64(9222246136947933184) ior 3)|word64(word64(9222246136947933184) ior 2)))
+        when OP_NEGATE then
+            if not peek(0) iand word64(9222246136947933184)<>word64(9222246136947933184) then
+                runtimeError("Operand must be a number.")
+                return INTERPRET_RUNTIME_ERROR
+            fi
+            pushx(numToValue(-valueToNum(popx())))
+        when OP_PRINT then
+            printValue(popx())
+            printf("\n")
+        when OP_JUMP then
+            offset := (frame^.ip+:=2;word16(frame^.ip[-2]<<8 ior frame^.ip[-1]))
+            frame^.ip +:= offset
+        when OP_JUMP_IF_FALSE then
+            offset := (frame^.ip+:=2;word16(frame^.ip[-2]<<8 ior frame^.ip[-1]))
+            if isFalsey(peek(0)) then
+                frame^.ip +:= offset
+            fi
+        when OP_LOOP then
+            offset := (frame^.ip+:=2;word16(frame^.ip[-2]<<8 ior frame^.ip[-1]))
+            frame^.ip -:= offset
+        when OP_CALL then
+            argCount := (frame^.ip++)^
+            if not callValue(peek(argCount),argCount) then
+                return INTERPRET_RUNTIME_ERROR
+            fi
+            frame := &vm.frames[vm.frameCount-1]
+        when OP_INVOKE then
+            method := ref ObjString(ref Obj(word64(((frame^.closure)^.function)^.chunk.constants.values[(frame^.ip++)^] iand inot word64(-9223372036854775808) ior word64(9222246136947933184))))
+            argCount := (frame^.ip++)^
+            if not invoke(method,argCount) then
+                return INTERPRET_RUNTIME_ERROR
+            fi
+            frame := &vm.frames[vm.frameCount-1]
+        when OP_SUPER_INVOKE then
+            method := ref ObjString(ref Obj(word64(((frame^.closure)^.function)^.chunk.constants.values[(frame^.ip++)^] iand inot word64(-9223372036854775808) ior word64(9222246136947933184))))
+            argCount := (frame^.ip++)^
+            superclass := ref ObjClass(ref Obj(word64(popx() iand inot word64(-9223372036854775808) ior word64(9222246136947933184))))
+            if not invokeFromClass(superclass,method,argCount) then
+                return INTERPRET_RUNTIME_ERROR
+            fi
+            frame := &vm.frames[vm.frameCount-1]
+        when OP_CLOSURE then
+            function := ref ObjFunction(ref Obj(word64(((frame^.closure)^.function)^.chunk.constants.values[(frame^.ip++)^] iand inot word64(-9223372036854775808) ior word64(9222246136947933184))))
+            closure := newClosure(function)
+            pushx(word64(word64(-9223372036854775808) ior word64(9222246136947933184) ior word64(closure)))
+            i := 0
+            while i<closure^.upvalueCount do
+                isLocal := (frame^.ip++)^
+                index := (frame^.ip++)^
+                if isLocal then
+                    closure^.upvalues[i] := captureUpvalue(frame^.slots+index)
+                else
+                    closure^.upvalues[i] := (frame^.closure)^.upvalues[index]
+                fi
+                i++
+            od
+        when OP_CLOSE_UPVALUE then
+            closeUpvalues(vm.stackTop-1)
+            popx()
+        when OP_RETURN then
+            result := popx()
+            closeUpvalues(frame^.slots)
+            vm.frameCount--
+            if vm.frameCount=0 then
+                popx()
+                return INTERPRET_OK
+            fi
+            vm.stackTop := frame^.slots
+            pushx(result)
+            frame := &vm.frames[vm.frameCount-1]
+        when OP_CLASS then
+            pushx(word64(word64(-9223372036854775808) ior word64(9222246136947933184) ior word64(newClass(ref ObjString(ref Obj(word64(((frame^.closure)^.function)^.chunk.constants.values[(frame^.ip++)^] iand inot word64(-9223372036854775808) ior word64(9222246136947933184))))))))
+        when OP_INHERIT then
+            superclass := peek(1)
+            if not isObjType(superclass,OBJ_CLASS) then
+                runtimeError("Superclass must be a class.")
+                return INTERPRET_RUNTIME_ERROR
+            fi
+            subclass := ref ObjClass(ref Obj(word64(peek(0) iand inot word64(-9223372036854775808) ior word64(9222246136947933184))))
+            tableAddAll(&(ref ObjClass(ref Obj(word64(superclass iand inot word64(-9223372036854775808) ior word64(9222246136947933184)))))^.methods,&subclass^.methods)
+            popx()
+        when OP_METHOD then
+            defineMethod(ref ObjString(ref Obj(word64(((frame^.closure)^.function)^.chunk.constants.values[(frame^.ip++)^] iand inot word64(-9223372036854775808) ior word64(9222246136947933184)))))
+        end switch
+    od
 end
