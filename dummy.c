6f5c6307a3aa48c4ce0ba2c0b23e379cb00dd7e5
sal55 <47458439+sal55@users.noreply.github.com>
2026-04-08 18:33:32 +0100

    Update dummy.c

--- dummy.c
+++ dummy.c
@@ -1,3 +1,9 @@
 //count characters in a string   
-int countchr(char *str, char chr)
-{int c=0,cnt=0;while(str[c]!='\0'){if(str[c]==chr){cnt++;}c++;}return cnt;}   
+int countchr(char *str, char chr) {
+	int c=0, cnt=0;
+	while (str[c]!='\0') {
+		if (str[c] == chr) cnt++;
+		c++;
+	}
+	return cnt;
+}
